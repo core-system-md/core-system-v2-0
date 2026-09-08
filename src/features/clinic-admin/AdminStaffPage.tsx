26a88217f92949b222a368e87012fa231b26629c
core-system-md <coresysyaz@gmail.com>
2026-09-08 23:48:35 +0300

    feat(admin): expose staff management from staff screen

--- src/features/clinic-admin/AdminStaffPage.tsx
+++ src/features/clinic-admin/AdminStaffPage.tsx
@@ -1,10 +1,14 @@
+import StaffManagement from './StaffManagement';
 import StaffPerformance from './StaffPerformance';
 import { PermissionGuard } from '@/core/permissions/PermissionGuard';
 
 export default function AdminStaffPage() {
   return (
     <PermissionGuard required="view_staff">
-      <StaffPerformance />
+      <div className="space-y-8">
+        <StaffManagement />
+        <StaffPerformance />
+      </div>
     </PermissionGuard>
   );
 }
